144f570c100019783c71208cbf05c3afc992beee
Shharder <era55@onet.pl>
2018-10-09 13:28:59 +0200

    Update

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: "page-about",
+  templateUrl: "about.html"
 })
 export class AboutPage {
-
-  constructor(public navCtrl: NavController) {
-
+  closeNews: boolean = true;
+  constructor(public navCtrl: NavController) {}
+  // ###Header###
+  help() {}
+  calendary() {}
+  // ###statement###
+  CloseStatement() {
+    this.closeNews = !this.closeNews;
+    console.log("dupa");
   }
-
+  // ###Weeks###
 }
