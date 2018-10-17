fb5b1b0760def102fb0849b03dedd4d238122254
ALONEK <alonek@github.com>
2018-10-17 11:19:06 +0200

    update-component

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { EventsWeekComponent } from './events-week/events-week';
+@NgModule({
+	declarations: [EventsWeekComponent],
+	imports: [],
+	exports: [EventsWeekComponent]
+})
+export class ComponentsModule { }
